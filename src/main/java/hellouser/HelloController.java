7bb8d9e9dca520665a26d81cabea1052c83e6b99
TsongaKing <phangi38@outlook.com>
2024-07-02 10:08:51 +0100

    Initial commit

---
+++ src/main/java/hellouser/HelloController.java
@@ -0,0 +1,84 @@
+package hellouser;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author phang
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+    private static final String IP_INFO_URL = "http://ipinfo.io/{ip}/json";
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=NewYork&units=metric&appid=75a8dfa1c63a0090b854207cf11fbe42";
+
+    private final String apiKey = "75a8dfa1c63a0090b854207cf11fbe42";
+
+    @GetMapping("/75a8dfa1c63a0090b854207cf11fbe42/hello-controller")
+    public HelloResponse hello(
+            @RequestParam("visitor_name") String visitorName,
+            HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Fetch location
+        IpInfo ipInfo = restTemplate.getForObject(IP_INFO_URL, IpInfo.class, clientIp);
+        String location = ipInfo.getCity();
+
+        // Fetch weather
+        WeatherResponse weatherResponse = restTemplate.getForObject(WEATHER_URL, WeatherResponse.class, location, apiKey);
+        String temperature = weatherResponse.getMain().getTemp().toString();
+
+        String greeting = "Hello, " + visitorName + "!, the temperature is " + temperature + " degrees Celsius in " + location;
+        return new HelloResponse(clientIp, location, greeting);
+    }
+}
+
+class IpInfo {
+    private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
+
+class WeatherResponse {
+    private Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    class Main {
+        private Double temp;
+
+        public Double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(Double temp) {
+            this.temp = temp;
+        }
+    }
+}
+
+    
+
---
+++ src/main/java/hellouser/HelloUser.java
@@ -0,0 +1,59 @@
+package hellouser;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author phang
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloUser {
+    @GetMapping("/api/hello-user")
+    public HelloResponse hello(
+            @RequestParam("visitor_name") String visitorName,
+            HttpServletRequest request) {
+        String clientIp = request.getRemoteAddr();
+        String location = "New York"; // Simplified for this example
+        String greeting = "Hello, " + visitorName + "!, the temperature is 11 degrees Celsius in " + location;
+        return new HelloResponse(clientIp, location, greeting);
+    }
+}
+
+class HelloResponse {
+    private String clientIp;
+    private String location;
+    private String greeting;
+
+    public HelloResponse(String clientIp, String location, String greeting) {
+        this.clientIp = clientIp;
+        this.location = location;
+        this.greeting = greeting;
+    }
+
+    public String getClientIp() {
+        return clientIp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
+
+
+
+    
+
