7a4b2c8d9cdc2e497976d3ca165e58fa51ffdd9f
TsongaKing <164667553+TsongaKing@users.noreply.github.com>
2024-07-03 16:32:55 +0200

    Update HelloController.java

--- src/main/java/hellouser/HelloController.java
+++ src/main/java/hellouser/HelloController.java
@@ -1,15 +1,9 @@
 package hellouser;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author phang
- */
 
+import hellouser.MyApiKeys.IpInfo;
+import hellouser.MyApiKeys.WeatherResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +20,7 @@ public class HelloController {
 
     @GetMapping("/api/hello-controller")
     public HelloResponse hello(
-            @RequestParam("Mark") String Mark,
+            @RequestParam("Phangi") String visitorName,
             HttpServletRequest request) {
         String clientIp = request.getRemoteAddr();
         RestTemplate restTemplate = new RestTemplate();
@@ -39,44 +33,8 @@ public class HelloController {
         WeatherResponse weatherResponse = restTemplate.getForObject(WEATHER_API_URL, WeatherResponse.class, location, apiKey);
         String temperature = weatherResponse.getMain().getTemp().toString();
 
-        String greeting = "Hello, " + Mark + "!, the temperature is " + temperature + " degrees Celsius in " + location;
+        String greeting = "Hello, " + visitorName + "!, the temperature is " + temperature + " degrees Celsius in " + location;
         return new HelloResponse(clientIp, location, greeting);
-    }
-}
-
-class IpInfo {
-    private String city;
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-}
-
-class WeatherResponse {
-    private Main main;
-
-    public Main getMain() {
-        return main;
-    }
-
-    public void setMain(Main main) {
-        this.main = main;
-    }
-
-    class Main {
-        private Double temp;
-
-        public Double getTemp() {
-            return temp;
-        }
-
-        public void setTemp(Double temp) {
-            this.temp = temp;
-        }
     }
 }
 
